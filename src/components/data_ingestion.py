998dbdae02c065feb93ef98f87d814f289c1c8e4
Abhinav Singh <abhinavishu0311@gmail.com>
2026-01-06 00:07:51 +0530

    Hyperparamter tunning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,7 @@ import sys
 from src.exception import CustomException # for daeling exceptions 
 from src.logger import logging 
 import pandas as pd 
+
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -19,13 +20,19 @@ def save_object(file_path, obj):
     except Exception as e:
        raise CustomException(e, sys)
 
-def evaluate_models(X_train, y_train,X_test,y_test,models):
+#we train the model
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-
+            para = param.get(list(models.keys())[i], {})
+            
+            gs=GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+            
+            model.set_params(**gs.best_params_) #we get the best params
             model.fit(X_train,y_train)
 
             #model.fit(X_train, y_train)  # Train model
